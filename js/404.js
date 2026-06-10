ca81b2da87a5d13e265ee3fb2a968d5e5e5090f7
_Spark <spark-aur@proton.me>
2026-06-10 15:39:08 +0300

    small changes

---
+++ js/404.js
@@ -0,0 +1,11 @@
+// i hate javascript with every fiber of my body
+// so the fact i have to write this to make the button on the 404 page work
+// is mental torture to me
+// god help us all
+
+document.getElementsByClassName("404button")[0].addEventListener("click", function() {
+  window.location.href = "index.html";
+});
+
+// that wasnt so hard actually
+// still hate js tho
